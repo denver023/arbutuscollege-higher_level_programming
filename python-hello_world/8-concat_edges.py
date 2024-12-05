d06893a47f43cda1f1269a0788ddef95d52fb225
denver <denvercayetano23@gmail.com>
2024-12-05 00:39:35 +0000

    Python Lesson

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+oop = str[39:66]
+w = str[108:112]
+python = str[0:7]
+
+print(oop + " " +  w + " " + python)
