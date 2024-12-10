1fdfd16713865b439a64ccd4a5f9e6f3c1a335ba
denver <denvercayetano23@gmail.com>
2024-12-10 00:38:19 +0000

    Python Lesson

---
+++ python-hello_world/7-edges_update.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton School"
+word_first_3 = word[10:13]
+word_last_2 = word[14:16]
+middle_word = word[11:15]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
