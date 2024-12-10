7444624630bcf639111ccaf9888651ed809f5b2d
denver <denvercayetano23@gmail.com>
2024-12-10 00:48:09 +0000

    Python Lesson

---
+++ python-hello_world/7-edge_update.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "School"
+word_first_3 = word[0:3]
+word_last_2 = word[4:7]
+middle_word = word[1:5]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
