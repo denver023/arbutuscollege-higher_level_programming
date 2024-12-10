6c6e4dd6dd0fcf1f9f1021116461bb1e4241558f
denver023 <denvercayetano23@gmail.com>
2024-12-09 16:42:59 -0800

    Python Lesson

--- python-hello_world/7-edges_update.py
+++ python-hello_world/7-edges_update.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-word = "Holberton School"
-word_first_3 = word[10:13]
-word_last_2 = word[14:16]
-middle_word = word[11:15]
+word = "School"
+word_first_3 = word[0:3]
+word_last_2 = word[4:6]
+middle_word = word[1:5]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
