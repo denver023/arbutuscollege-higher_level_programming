d45d056aa673bcb909afdd0f654da23511f275c8
denver <denvercayetano23@gmail.com>
2024-12-12 07:54:58 +0000

    Python Lesson

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    # If tuple_a has fewer than 2 elements, use 0 for missing elements
+    a1, a2 = (tuple_a + (0, 0))[:2]
+    # If tuple_b has fewer than 2 elements, use 0 for missing elements
+    b1, b2 = (tuple_b + (0, 0))[:2]
+    
+    # Return a tuple with the sums of the corresponding elements
+    return (a1 + b1, a2 + b2)
