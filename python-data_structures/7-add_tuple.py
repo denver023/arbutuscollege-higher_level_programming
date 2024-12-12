c338610f7f72d377dbd437959f1af015296f5a44
denver023 <denvercayetano23@gmail.com>
2024-12-12 00:04:00 -0800

    Update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        tuple_a = tuple_a + (0,)
-    if len(tuple_b) < 2:
-        tuple_b = tuple_b + (0,)
-
-    # This line should be completely empty without spaces:
-    
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    # If tuple_a has fewer than 2 elements, use 0 for missing elements
+    a1, a2 = (tuple_a + (0, 0))[:2]
+    # If tuple_b has fewer than 2 elements, use 0 for missing elements
+    b1, b2 = (tuple_b + (0, 0))[:2]
+    # Return a tuple with the sums of the corresponding elements
+    return (a1 + b1, a2 + b2)
