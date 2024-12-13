36f08d29a13e4e4d695176e7b5da7a1858448723
denver <denvercayetano23@gmail.com>
2024-12-13 22:41:38 +0000

    Python Lesson

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 class Rectangle:
+    """A class representing a rectangle."""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
