49fce2eb795d1024cbec03062cc6bac129e76526
denver <denvercayetano23@gmail.com>
2025-01-24 00:16:02 +0000

    SQL Queries Test10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
