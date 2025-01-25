c0c85ac9e98eac83a792044bb56e6e61dca525b1
denver023 <denvercayetano23@gmail.com>
2025-01-24 20:35:30 -0800

    SQL Queries Test 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,7 @@
--- This query lists all genres for the show 'Dexter' and sorts them in ascending order by genre name
-SELECT tv_genres.name AS genre FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name ASC;
+-- This query lists all genres of the show 'Dexter' and sorts them in ascending order by genre name.
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
