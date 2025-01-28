6cc002674d097371c3c49854bd9db23674a87d54
denver023 <denvercayetano23@gmail.com>
2025-01-28 15:45:55 -0800

    Update 8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,23 +11,23 @@ from model_state import Base, State
 if __name__ == "__main__":
    # Create engine to connect to MySQL server
    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                         format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                         pool_pre_ping=True)
-
+                          format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                          pool_pre_ping=True)
+   
    # Create session factory bound to the engine
    Session = sessionmaker(bind=engine)
-
+   
    # Create a session
    session = Session()
-
+   
    # Query first State object
    first_state = session.query(State).order_by(State.id).first()
-
+   
    # Display result
    if first_state:
-       print("{}: {}".format(first_state.id, first_state.name))
+      print("{}: {}".format(first_state.id, first_state.name))
    else:
-       print("Nothing")
-
+      print("Nothing")
+      
    # Close session
-   session.close()
+session.close()
