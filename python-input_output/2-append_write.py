382bb6249000e9a06aec539729b7480875b39686
denver <denvercayetano23@gmail.com>
2025-01-28 06:12:52 +0000

    Python - Input/Output

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def append_write(filename="", text=""):
+    """
+    Appends a string to the end of a text file (UTF8) and returns the number of characters added.
+
+    Args:
+        filename (str): The name of the file to append to. Defaults to an empty string.
+        text (str): The string to append to the file. Defaults to an empty string.
+
+    Returns:
+        int: The number of characters added.
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        return file.write(text)
