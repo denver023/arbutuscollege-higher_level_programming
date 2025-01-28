506c9234d5ca80672f24856e014cd455e8a27a73
denver <denvercayetano23@gmail.com>
2025-01-28 06:32:10 +0000

    Python - Input/Output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """
-This script adds all arguments to a Python list and saves them to a JSON file.
+This script adds all arguments passed to it via the command line
+to a list stored in a JSON file named 'add_item.json'.
+If the file does not exist, it will be created.
+Each time the script is run, the arguments are appended to the list.
 """
-
 import sys
-from os.path import exists
-from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
+from 5-save_to_json_file import save_to_json_file
 
-filename = "add_item.json"
+def add_items():
+    try:
+        # Try loading the existing list from the file
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        # If the file doesn't exist, start with an empty list
+        items = []
+    
+    # Add command-line arguments to the list
+    items.extend(sys.argv[1:])
+    
+    # Save the updated list back to the JSON file
+    save_to_json_file(items, "add_item.json")
 
-# Load existing data if the file exists, otherwise start with an empty list
-if exists(filename):
-    items = load_from_json_file(filename)
-else:
-    items = []
-
-# Add arguments to the list
-items.extend(sys.argv[1:])
-
-# Save the updated list back to the file
-save_to_json_file(items, filename)
+if __name__ == "__main__":
+    add_items()
