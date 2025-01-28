4a875fdd5a4d76dbddd7d2bc573b62ba9f1935a8
denver023 <denvercayetano23@gmail.com>
2025-01-28 15:51:06 -0800

    Update 8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,26 +8,27 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
-   # Create engine to connect to MySQL server
-   engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                         format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                         pool_pre_ping=True)
+    # Create engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                             format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                             pool_pre_ping=True)
 
-   # Create session factory bound to the engine
-   Session = sessionmaker(bind=engine)
+    # Create session factory bound to the engine
+    Session = sessionmaker(bind=engine)
 
-   # Create a session
-   session = Session()
+    # Create a session
+    session = Session()
 
-   # Query first State object
-   first_state = session.query(State).order_by(State.id).first()
+    # Query first State object
+    first_state = session.query(State).order_by(State.id).first()
 
-   # Display result
-   if first_state:
-       print("{}: {}".format(first_state.id, first_state.name))
-   else:
-       print("Nothing")
+    # Display result
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
 
-   # Close session
-   session.close()
+    # Close session
+    session.close()
