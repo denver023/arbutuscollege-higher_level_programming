e7bcc0de2048fa222786a0dbe7405a0d98e9f2a5
denver <denvercayetano23@gmail.com>
2025-01-28 06:00:22 +0000

    Python - Input/Output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This module provides a function to read and print the content of a text file.
+"""
 
 def read_file(filename=""):
     """
@@ -12,6 +15,3 @@ def read_file(filename=""):
     """
     with open(filename, mode='r', encoding='utf-8') as file:
         print(file.read(), end='')
-
-# Example usage:
-# read_file("my_file_0.txt")
