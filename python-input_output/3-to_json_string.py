90c2aea99e9df65be6c2141bbdfcc18cdcfac5f1
denver023 <denvercayetano23@gmail.com>
2025-01-27 22:20:40 -0800

    Update 3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+"""
+This module provides a function to return the JSON representation of an object.
+"""
+
 import json
 
 def to_json_string(my_obj):
     """
-    Returns the JSON representation of an object as a string.
+    Returns the JSON representation of an object (string).
 
     Args:
         my_obj: The object to serialize to JSON.
 
     Returns:
-        str: The JSON string representation of the object.
+        str: The JSON representation of the object.
     """
     return json.dumps(my_obj)
