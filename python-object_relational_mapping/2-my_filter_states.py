847d3319841bf3b282d1b9ee99632aeb1eae69dd
denver <denvercayetano23@gmail.com>
2025-01-28 20:17:02 +0000

    Python - Object-relational mapping

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Script that displays all values in the states table
+where name matches the argument
+"""
+import MySQLdb
+import sys
+
+
+def filter_states_by_input(username, password, db_name, state_name):
+    """
+    Lists all states where name matches the argument
+    Args:
+        username (str): MySQL username
+        password (str): MySQL password
+        db_name (str): Database name
+        state_name (str): Name of state to search for
+    """
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    # Create cursor to execute queries
+    cursor = db.cursor()
+
+    # Create and execute query using format
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    cursor.execute(query.format(state_name))
+
+    # Fetch all results
+    states = cursor.fetchall()
+
+    # Print results
+    for state in states:
+        print(state)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 5:
+        filter_states_by_input(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
