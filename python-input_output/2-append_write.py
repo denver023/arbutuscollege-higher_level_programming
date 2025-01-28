7049840941ce6dd39587af3a41ae484dd4558c4b
denver023 <denvercayetano23@gmail.com>
2025-01-27 22:19:23 -0800

    Update 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module provides a function to append a string to the end of a text file in UTF-8 encoding.
+"""
+
 def append_write(filename="", text=""):
     """
     Appends a string to the end of a text file (UTF8) and returns the number of characters added.
