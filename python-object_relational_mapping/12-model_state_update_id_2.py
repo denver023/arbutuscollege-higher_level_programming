e10fb8f9e9e11593ca2e61392d8f7db9095c2818
denver023 <denvercayetano23@gmail.com>
2025-01-28 16:19:50 -0800

    Update 12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,15 +10,15 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-   engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                         format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                         pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                             format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                             pool_pre_ping=True)
 
-   Session = sessionmaker(bind=engine)
-   session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-   state = session.query(State).filter(State.id == 2).first()
-   state.name = "New Mexico"
-   session.commit()
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
 
-   session.close()
+    session.close()
