502a1f59e695bba8f5858a2688770cd1785883eb
denver023 <denvercayetano23@gmail.com>
2025-01-28 16:00:16 -0800

    Update 10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -10,18 +10,18 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-   engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                           pool_pre_ping=True)
 
-   Session = sessionmaker(bind=engine)
-   session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-   state = session.query(State).filter(State.name == sys.argv[4]).first()
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
 
-   if state:
-       print("{}".format(state.id))
-   else:
-       print("Not found")
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
 
-   session.close()
+    session.close()
