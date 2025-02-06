3b39b2448cb049aabcbe57c48ff7466e242f5bcd
denver <denvercayetano23@gmail.com>
2025-02-06 23:07:54 +0000

    JavaScript warm up

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const myC = "C is fun";
-const myPython = "Python is cool";
-const myJS = "JavaScript is amazing";
+const myC = 'C is fun';
+const myPython = 'Python is cool';
+const myJS = 'JavaScript is amazing';
 
 console.log(myC);
 console.log(myPython);
